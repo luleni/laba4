545c6c512ac05a7c49579778f7c3ac63d57b1060
luleni <lenapetrovkhyh@gmail.com>
2025-06-08 17:11:10 +0700

    ex1

--- ex1.py
+++
@@ -1,7 +0,0 @@
-import random
-array = []
-for i in range(10):
-    j = random.randint(-10,10)
-    array.append(j)
-for i in array:
-    print(i)
--- ex2.py
+++
@@ -1,8 +0,0 @@
-array = []
-print('Введите 14 чисел:')
-for i in range(14):
-    j = int(input())
-    array.append(j)
-array.append(sum(array))
-for i in array:
-    print(i)
